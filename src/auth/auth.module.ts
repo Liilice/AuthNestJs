3a8e17aa2559938c78418cf12e66b8245cf28502
Liilice <alice.zheng@epitech.eu>
2025-06-11 12:27:35 +0200

    FEAT : auth guard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
-import * as dotenv from 'dotenv';
 import { JwtModule } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
+import { AuthGuard } from './auth.guard';
 dotenv.config();
 @Module({
   imports: [UsersModule, JwtModule.register({ global: true , secret: process.env.JWT_SECRET})],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, AuthGuard],
 })
 export class AuthModule {}
